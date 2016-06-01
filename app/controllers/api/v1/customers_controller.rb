b6b746ee1c774bf3f094088b567ce019410c9b04
Alireza Andersen <alireza.andersen@gmail.com>
2016-05-31 22:21:55 -0600

    create customer record endpoints

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -2,6 +2,34 @@ module Api
   module V1
     class CustomersController < ApiController
 
+      respond_to :json
+
+      def index
+        respond_with Customer.all
+      end
+
+      def show
+        respond_with Customer.find(params[:id])
+      end
+
+      def find
+        respond_with Customer.find_by(customer_params)
+      end
+
+      def find_all
+        respond_with Customer.where(customer_params)
+      end
+
+      def random
+        respond_with Customer.all.sample
+      end 
+
+      private
+
+      def customer_params
+        params.permit(:id, :first_name, :last_name, :created_at, :updated_at)
+      end
+
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,19 +3,29 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json } do
     namespace :v1 do
 
-      resources :merchants, only: [:index, :show], module: "merchants" do
+      resources :merchants, only: [:index, :show], module: 'merchants' do
 
         collection do
-          get "/find",         to: "merchants#find"
-          get "/find_all",     to: "merchants#find_all"
-          get "/random",       to: "merchants#random"
+          get '/find',         to: 'merchants#find'
+          get '/find_all',     to: 'merchants#find_all'
+          get '/random',       to: 'merchants#random'
         end
 
         member do
-          get "/items",                           to: "merchant_items#index"
-          get "/invoices",                        to: "merchant_invoices#index"
+          get '/items',                           to: 'merchant_items#index'
+          get '/invoices',                        to: 'merchant_invoices#index'
         end
       end
+
+      resources :customers, only: [:index, :show], module: 'customers' do
+
+        collection do
+          get '/find',         to: 'customers#find'
+          get '/find_all',     to: 'customers#find_all'
+          get '/random',       to: 'customers#random'
+        end
+      end
+
     end
   end
 end
